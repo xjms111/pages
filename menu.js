f64de63a34c23573bae37c2f159f5b2ed232d589
xjms111 <a.dejewski07@gmail.com>
2025-04-24 12:29:55 +0200

    Add files via upload

--- menu.js
+++ menu.js
@@ -6,9 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.getElementById("hamburger");
     const menu = document.getElementById("menu");
 
-    hamburger.addEventListener("click", function () {
-        menu.classList.toggle("show");
-    });
+    if (hamburger && menu) {
+        hamburger.addEventListener("click", function () {
+            menu.classList.toggle("show");
+        });
+    }
 });
 
 
